629e6c8aedf74937254f9715c6cf8b0d90e4d61c
Sun <sun@example.com>
2026-07-30 14:08:07 +0700

    Update Azure AD Client ID

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@
 // TODO: Replace with your actual Client ID and Tenant ID once you register the App in Entra ID
 export const msalConfig = {
     auth: {
-        clientId: "f5171fbf-14a9-4631-aa48-da8dc6a38a5c",
+        clientId: "7486bca3-4f63-4c52-ba5c-f6e396692ee5",
         authority: "https://login.microsoftonline.com/cf10c7a9-863e-4b9e-8564-28f10b151033", 
         redirectUri: window.location.origin,
     },
