2d17bd084e37e9c6bb09719968aaf396e8639cd2
Sun <sun@example.com>
2026-08-06 11:34:03 +0700

    fix: redirect to app after microsoft logout

--- src/authConfig.js
+++ src/authConfig.js
@@ -6,6 +6,7 @@ export const msalConfig = {
         clientId: "7486bca3-4f63-4c52-ba5c-f6e396692ee5",
         authority: "https://login.microsoftonline.com/cf10c7a9-863e-4b9e-8564-28f10b151033",
         redirectUri: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
     },
     cache: {
         cacheLocation: "localStorage",
